d489d91689fa293a7f1b6dc70e8e1632cd08c15b
RafaS92 <manrvaldez..92@gmail.com>
2020-08-15 23:58:47 -0500

    fetching the list of movies on rows working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Row from "./components/Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
