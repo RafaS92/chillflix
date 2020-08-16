5e48be5fdeef863d8cf60603c751ba3e3bc36b45
RafaS92 <manrvaldez..92@gmail.com>
2020-08-15 23:30:47 -0500

    axios file setted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Row from "./components/Row";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+    </div>
+  );
 }
 
 export default App;
