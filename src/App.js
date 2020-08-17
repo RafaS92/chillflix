c119c60d8592ce9a34d09ff74ed7a17685f21561
RafaS92 <manrvaldez..92@gmail.com>
2020-08-17 07:26:11 -0500

    movies trailers working

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Nav />
       <Banner />
       <Row
-        title="NETFLIX ORIGINALS"
+        title="CHILLFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
